dbe1f491b9c0874ce30d54d4da47d737ac6ed4d2
Howard Tran <hieu.tranthanh0706@gmail.com>
2024-05-28 22:47:53 +0700

    feat: add Hero and background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import Hero from "@/components/hero";
+import { Spotlight } from "@/components/ui/Spotlight";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import Hero from "@/components/ui/Hero";
 
 const Home = () => {
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "About",
+      link: "/about",
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+    },
+  ];
   return (
-    <div className="relative bg-black-100">
+    <div className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <Hero />
+      <FloatingNav navItems={navItems} />
     </div>
   );
 };
--- src/components/hero.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Hero = () => {
-  return <div>Hero header</div>;
-};
-
-export default Hero;
